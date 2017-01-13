1e857bacab2dbf0d2c885cda893376b9894c04fb
Ilya Morozov <imorozov@n-core.ru>
2017-01-13 08:42:49 +0300

    UpdateService spec

--- spec/models/update_service_spec.rb
+++ spec/models/update_service_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe UpdateService, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.run starts updated with interval' do
+    it 'calls scheduler.interval with update in block and joins' do
+      expect_any_instance_of(Rufus::Scheduler).to receive(:interval) do |&block|
+        block.call
+      end
+      expect(UpdateService).to receive(:update)
+      expect_any_instance_of(Rufus::Scheduler).to receive(:join)
+      UpdateService.run
+    end
+  end
+
+  describe '.update' do
+    context 'when there is no current override' do
+      let(:value) { '1' }
+
+      it 'gets, saves, and broadcasts currency' do
+        expect(RemoteCurrencyProviders).to receive(:get_currency).and_return(value)
+        expect(CurrencyRepository).to receive(:save).with(value)
+        expect(CurrencyChannel).to receive(:broadcast).with(value)
+        UpdateService.update
+      end
+    end
+  end
 end
