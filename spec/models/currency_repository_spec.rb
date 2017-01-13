00b040d89c63579988af555336b5844de125299d
Ilya Morozov <imorozov@n-core.ru>
2017-01-13 07:36:10 +0300

    currency_repository spec

--- spec/models/currency_repository_spec.rb
+++ spec/models/currency_repository_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
-RSpec.describe CurrencyRepository, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe CurrencyRepository do
+  after(:each) { Redis.current.flushdb }
+
+  let(:value) { '1' }
+
+  describe '.get' do
+    it 'gets currency value from redis' do
+      Redis.current.set(described_class::KEY, value)
+      expect(described_class.get).to eq(value)
+    end
+
+    it 'returns nil if there is no value' do
+      expect(described_class.get).to be_nil
+    end
+  end
+
+  describe '.save' do
+    it 'saves value to redis under ::KEY' do
+      described_class.save(value)
+      expect(described_class.get).to eq(value)
+    end
+  end
 end
