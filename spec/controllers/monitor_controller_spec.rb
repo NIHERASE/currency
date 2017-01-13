e3a0c1deb236020f88fe28982d6e301de06248fd
Ilya Morozov <imorozov@n-core.ru>
2017-01-13 09:35:09 +0300

    monitor controller spec

--- spec/controllers/monitor_controller_spec.rb
+++ spec/controllers/monitor_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe MonitorController, type: :controller do
+  describe 'index' do
+    let(:value) { '1' }
 
+    it 'sets @ruble_rate from CurrencyRepository' do
+      expect(CurrencyRepository).to receive(:get).and_return(value)
+      get :index
+      expect(assigns(:ruble_rate)).to eq(value)
+    end
+
+    it 'sets @ruble_rate to __ if nothing is in CurrencyRepository' do
+      expect(CurrencyRepository).to receive(:get).and_return(nil)
+      get :index
+      expect(assigns(:ruble_rate)).to eq('__')
+    end
+  end
 end
